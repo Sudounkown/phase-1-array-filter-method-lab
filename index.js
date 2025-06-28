1bf1aeac368f02bb38ae807e678b9cf0af333fe0
Roy Koigu <roykoigu@protonmail.com>
2025-06-28 13:41:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+const drivers  = [
+ {
+    "name": "Bob",
+    "hometown": "big boy hometown"
+ },
+  {
+    "name": "Teressa",
+    "hometown": "big boy hometown"
+ },
+  {
+    "name": "Kate",
+    "hometown": "small boy hometown"
+ },
+  {
+    "name": "Andy",
+    "hometown": "hometown pending"
+ },
+]
+
+const findMatching = (drivers, value) => {
+    return drivers.filter(item => item.toLowerCase() === value.toLowerCase())
+}
+
+const fuzzyMatch = (drivers, value) => {
+    const lower = value.toLowerCase()
+    return drivers.filter(item => item.toLowerCase().startsWith(lower))
+}
+
+const matchName = (drivers, value) => {
+    const lower = value.toLowerCase()
+    return drivers.filter(item => item.name.toLowerCase() === lower)
+}
